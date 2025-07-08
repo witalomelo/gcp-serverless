512bcd8078d43a0f88775205aa9cb110a74068c6
witalomelo <witalovmelo@gmail.com>
2025-07-07 21:36:32 -0300

    correcao recebeAtividade

--- funcoes/recebeAtividade.js
+++ funcoes/recebeAtividade.js
@@ -1,6 +1,6 @@
 const pubsub = require('./pubsub')
 
-module.exports = function recebeAtividade(requisicao, resposta) {
+module.exports = async function recebeAtividade(requisicao, resposta) {
     const resultado = await pubsub(requisicao.body, 'atividades')
     console.log(requisicao.body)
     resposta.send(resultado)
