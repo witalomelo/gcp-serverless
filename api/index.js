66aca4a8eae1b5be616c0801de8f710e3f60b939
witalomelo <witalovmelo@gmail.com>
2025-07-10 20:53:44 -0300

    adiciona autenticação básica à API e inclui a dependência 'basic-auth'

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
 const koa = require("koa");
+const conversor = require('basic-auth')
 const processador = require("koa-bodyparser");
 const aplicacao = new koa();
 const pesquisar = require("../bigquery/pesquisar");
 
 aplicacao.use(processador())
 aplicacao.use(async function (contexto) {
+    const usuarioEsenha = conversor.parse(contexto.request.headers.authorization) 
+    const USUARIO = process.env.USUARIO
+    const SENHA = process.env.SENHA
+
+    if (USUARIO !== usuarioEsenha.name || SENHA !== usuarioEsenha.pass) {
+        contexto.body = {
+            mensagem: "Usuário ou senha inválidos"
+        }
+        return
+    }
 
     const corpoDaRequisicao = contexto.request.body
     contexto.status = 200;
@@ -14,3 +25,4 @@ aplicacao.use(async function (contexto) {
 aplicacao.listen(3000);
 
 console.log("API rodando na porta 3000");
+
